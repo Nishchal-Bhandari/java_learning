fcf81c642962a44f13f93d13253bbbeffc21bad5
Nishchal-Bhandari <nishchalbhandari18@gmail.com>
2025-08-05 19:34:00 +0530

    added new files

---
+++ hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int aa=0b100+0xFFF;
+        if(true)
+        {
+            System.out.println(aa);
+            
+        }
+        
+    }
+    }
