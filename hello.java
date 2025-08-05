3b08cd199439a2fef41f210bd97bf7f2755b0c97
Nishchal-Bhandari <nishchalbhandari18@gmail.com>
2025-08-05 19:58:26 +0530

    made some changes

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
     public static void main(String a[])
     {
-        int aa=0b100+0xFFF;
+        int aa=0b1000+0xFFF;
         if(true)
         {
             System.out.println(aa);
